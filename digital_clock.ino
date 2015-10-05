005ab09f7873626cdee68418c1badcb42333a9b1
sydneytheamazing <sydyium@icloud.com>
2015-10-05 10:21:06 -0500

    new code
    changed the code to shorter code

--- digital_clock.ino
+++ digital_clock.ino
@@ -3,15 +3,21 @@
 
 #include "SevSeg.h"
 
+SevSeg sevseg;
+
 #define d1 13
 #define a 2
 
 void setup() {
   // put your setup code here, to run once:
-
+byte numDigits = 4;
+   byte digitPins[] = {2, 3, 4, 5};
+   byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+   sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+sevseg.setNumber(3141,3); // Displays '3.141'
+sevseg.refreshDisplay();
 }
