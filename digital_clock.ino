b823c8df7a8be1d45a93ae7d91f4daa25ef90601
sydneytheamazing <sydyium@icloud.com>
2015-10-07 10:39:55 -0500

    timer code
    I changed the code to where we can set it to make it a clock. it will
    tell time if you set if you set the buttons

--- digital_clock.ino
+++ digital_clock.ino
@@ -3,21 +3,55 @@
 
 #include "SevSeg.h"
 
-SevSeg sevseg;
-
 #define d1 13
-#define a 2
+#define a 0
+#define f 7
+#define d2 12
+#define d3 11
+#define b 1
+#define e 6
+#define d 5
+#define dp 9
+#define c 4
+#define g 8 
+#define d4 10 
+#define HOUR_BUTTON 2
+#define MINUTE_BUTTON 3
+
+volatile int hour = 1;
+volatile int minute = 0;
+SevSeg sevseg;
 
 void setup() {
   // put your setup code here, to run once:
-byte numDigits = 4;
-   byte digitPins[] = {2, 3, 4, 5};
-   byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
-   sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  
+  pinMode(HOUR_BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(HOUR_BUTTON), hourUp, RISING);
+  pinMode(MINUTE_BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(MINUTE_BUTTON), minuteUp, RISING);
+
+  byte numDigits = 4;
+  byte digitPins[] = {d1, d2, d3, d4};
+  byte segmentPins[] = {a, b, c, d, e, f, g, dp};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-sevseg.setNumber(3141,3); // Displays '3.141'
-sevseg.refreshDisplay();
+  sevseg.setNumber(hour * 100 + minute, 3);
+  sevseg.refreshDisplay();
+}
+
+void hourUp() {
+  hour = hour + 1;
+  if (hour > 12) {
+    hour = 1;
+  }
+}
+void minuteUp() {
+  minute = minute + 1;
+  if (minute > 59) {
+    minute = 0;
+    hourUp();
+  }
 }
